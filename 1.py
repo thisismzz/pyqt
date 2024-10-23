31da9df7de1d526aa098fb6e2c59e049924dfa0e
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2024-10-23 17:37:44 +0330

    session 1 completed

--- 1.py
+++ 1.py
@@ -187,3 +187,60 @@ from time import sleep
 # sys.exit(app.exec_())
 
 #___________________________________________
+
+# class Window(QWidget):
+    
+#     def __init__(self):
+#         global buttongroup
+#         QWidget.__init__(self)
+#         layout = QGridLayout()
+#         self.setLayout(layout)
+#         self.resize(500,500)
+#         button1 = QPushButton("button1")
+#         layout.addWidget(button1)
+#         button2 = QPushButton("button2")
+#         layout.addWidget(button2)
+#         button3 = QPushButton("button3")
+#         layout.addWidget(button3)
+#         buttongroup = QButtonGroup()
+#         buttongroup.addButton(button1,1)
+#         buttongroup.addButton(button2,2)
+#         buttongroup.addButton(button3,3)
+#         buttongroup.buttonClicked[int].connect(self.on_button)
+        
+#     def on_button(self,id):
+#         print("number %s clicked" %id)
+        
+
+# app = QApplication(sys.argv)
+# screen = Window()
+# screen.show()
+# sys.exit(app.exec_())
+
+#_______________________________________
+
+# class Window(QWidget):
+    
+#     def __init__(self):
+#         QWidget.__init__(self)
+#         layout = QGridLayout()
+#         self.setLayout(layout)
+#         self.resize(400,400)
+#         box = QGroupBox("group box")
+#         layout.addWidget(box)
+#         box.setCheckable(True)
+#         sublayout = QGridLayout()
+#         box.setLayout(sublayout)
+#         button1 = QPushButton("button 1")
+#         sublayout.addWidget(button1)
+#         button2 = QPushButton("button 2")
+#         sublayout.addWidget(button2)
+
+# app = QApplication(sys.argv)
+# screen = Window()
+# screen.show()
+# sys.exit(app.exec_())       
+
+#________________________________________
+
+
